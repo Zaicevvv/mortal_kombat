f164ea9894525c807834ffb50952c91966fdbde0
Vladimir <vladimir.zaitsev@jointoit.com>
2023-09-21 11:46:14 +0300

    create-react-app + react-router-dom (for pages navigation) + react-transition-group (for triggering animations) + framer-motion (for smooth page transition) + characters images

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import noob from './assets/noob.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">hello</header>
+      <img alt="character" src={noob} />
+    </div>
+  )
+}
+
+export default App
